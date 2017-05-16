4355d7f27a9cedf968e0c62d4974fee48c85479e
Ludivine Stofer <LudivineStofer@gmail.com>
2017-05-16 17:43:03 +0200

    Leaflet fonctionne

--- index.js
+++ index.js
@@ -3,31 +3,28 @@ var M = {
   data: {}, dataSeries: [],
 };
 
-// Définition de la carte en SVG avec le cadre notamment
-function main(){
-  M.svg = d3.select("svg.choropleth");
-  M.width = M.svg.attr('width');
-  M.height = M.svg.attr('height');
+// Ouverture de la carte avec Leaflet
+M.map = L.map("mapdiv",{center:[46.8, 8.3],zoom:8});
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(M.map);
 
-// Définition d'une géométrie géoJSON en SVG
-  M.path = d3.geoPath();
+// Il faut créer une sorte de boucle (queue) qui permet de charger les données ainsi que les shapefiles
+d3.queue()
+  .defer(
+    d3.json,
+    "https://cdn.rawgit.com/christiankaiser/d3-topojson-choropleth/ee12f6a108eddaa9cd119866b2e9cd52bb450cbc/data/vec200-topo.json"
+  )
+  .defer(
+    d3.tsv,
+    "https://cdn.rawgit.com/christiankaiser/d3-topojson-choropleth/ee12f6a108eddaa9cd119866b2e9cd52bb450cbc/data/pop-fem-2034-2015.tsv",
+    function(d){
+      M.data[d.id] = d;
+      M.dataSeries.push(parseFloat(d.p_fem_singl_2034))
+    }
+  )
+  .await(drawMap);
 
-  // Il faut créer une sorte de boucle (queue) qui permet de charger les données ainsi que les shapefiles
-  d3.queue()
-    .defer(
-      d3.json,
-      "https://cdn.rawgit.com/christiankaiser/d3-topojson-choropleth/ee12f6a108eddaa9cd119866b2e9cd52bb450cbc/data/vec200-topo.json"
-    )
-    .defer(
-      d3.tsv,
-      "https://cdn.rawgit.com/christiankaiser/d3-topojson-choropleth/ee12f6a108eddaa9cd119866b2e9cd52bb450cbc/data/pop-fem-2034-2015.tsv",
-      function(d){
-        M.data[d.id] = d;
-        M.dataSeries.push(parseFloat(d.p_fem_singl_2034))
-      }
-    )
-    .await(drawMap);
-}
 
 
 function drawMap(error, data){
@@ -38,62 +35,73 @@ function drawMap(error, data){
   // to fit the map into the SVG view frame.
   // In a first step, we compute the transform parameters.
 
-  // Compute the scale of the transform
-  var scaleX = M.width / (M.bbox[2] - M.bbox[0]),
-      scaleY = M.height / (M.bbox[3] - M.bbox[1]);
-  var scale = Math.min(scaleX, scaleY); // Pour garder la même échelle en x et en y afin d'éviter les déformations
+  M.communesOverlay = L.d3SvgOverlay(function(sel, proj){
+    var features = sel.selectAll('path')
+    .data(topojson.feature(data, data.objects.communes).features) // Ajout des communes
 
-  var dx = -1 * scale * M.bbox[0];
-  var dy = scale * M.bbox[1] + parseFloat(M.height);
+    features
+      .enter()
+      .append('path')
+      .attr('stroke','white')
+      .attr('fill', 'red')
+      .attr('fill-opacity', 0.5)
+      .attr('d', proj.pathFromGeojson);
 
-  M.map = M.svg.append('g')
-    .attr('class', 'map')
-    .attr(
-      'transform',
-      'matrix('+scale+' 0 0 -'+scale+' '+dx+' '+dy+')'
-    );
+  });
 
-  // Mise en classe de la carte avec Jenks.
-  M.brew = new classyBrew();
-  M.brew.setSeries(M.dataSeries);
-  M.brew.setNumClasses(6);
-  M.brew.setColorCode('PuBu');
-  M.breaks = M.brew.classify('jenks');
+  M.communesOverlay.addTo(M.map)
 
-// Sélection des classes et des couleurs
-  M.color = d3.scaleThreshold()
-    .domain(M.breaks.slice(1,6))
-    .range(M.brew.getColors());
-
-  // Transformation des features en geoJSON et en SVG
-  M.map
-    .append('g').attr('class', 'communes')
-    .selectAll('path')
-    .data(topojson.feature(data, data.objects.communes).features) // Utilisation et conversion du TopoJSON
-    .enter()
-    .append('path')
-    .attr('fill', function(d){
-      return M.data[d.properties.id] ?
-        M.color(M.data[d.properties.id].p_fem_singl_2034) :
-        '#fff'; // Code couleur pour les données manquantes. 
-    })
-    .attr('d', M.path);
-
-  // Limites des cantons tracées en blanc. Attention le stroke-width est en mètres !!
-  M.map
-    .append('g').attr('class', 'cantons')
-    .selectAll('path')
-    .data(topojson.feature(data, data.objects.cantons).features)
-    .enter()
-    .append('path')
-    .attr('stroke', '#fff').attr('stroke-width', '200')
-    .attr('fill', 'none').attr('d', M.path);
-
-  // La couche des lacs est en dernier afin qu'ils apparaissent dessus.
-  M.map
-    .append('g').attr('class', 'lacs')
-    .selectAll('path')
-    .data(topojson.feature(data, data.objects.lacs).features)
-    .enter().append('path')
-    .attr('fill', '#777').attr('d', M.path);
 }
+
+
+
+//   // Mise en classe de la carte avec Jenks.
+//   M.brew = new classyBrew();
+//   M.brew.setSeries(M.dataSeries);
+//   M.brew.setNumClasses(6);
+//   M.brew.setColorCode('PuBu');
+//   M.breaks = M.brew.classify('jenks');
+//
+// // Sélection des classes et des couleurs
+//   M.color = d3.scaleThreshold()
+//     .domain(M.breaks.slice(1,6))
+//     .range(M.brew.getColors());
+//
+//   // Transformation des features en geoJSON et en SVG
+//   M.map
+//     .append('g').attr('class', 'communes')
+//     sel.selectAll('path')
+//     .data(topojson.feature(data, data.objects.communes).features) // Utilisation et conversion du TopoJSON
+//     .enter()
+//     .append('path')
+//     .attr('fill', function(d){
+//       return M.data[d.properties.id] ?
+//         M.color(M.data[d.properties.id].p_fem_singl_2034) :
+//         '#fff'; // Code couleur pour les données manquantes.
+//     })
+//     .attr('d', M.path);
+//
+//   // Limites des cantons tracées en blanc. Attention le stroke-width est en mètres !!
+//   M.map
+//     .append('g').attr('class', 'cantons')
+//     .selectAll('path')
+//     .data(topojson.feature(data, data.objects.cantons).features)
+//     .enter()
+//     .append('path')
+//     .attr('stroke', '#fff').attr('stroke-width', '200')
+//     .attr('fill', 'none').attr('d', M.path);
+//
+//   // La couche des lacs est en dernier afin qu'ils apparaissent dessus.
+//   M.map
+//     .append('g').attr('class', 'lacs')
+//     .selectAll('path')
+//     .data(topojson.feature(data, data.objects.lacs).features)
+//     .enter().append('path')
+//     .attr('fill', '#777').attr('d', M.path);
+// }
+//
+// // Utilise la fonction D3 avec un JSON
+// d3.json('vec200-cantons-topo.json', function(data){
+//   vec200_topojson = data;
+//   cantonsOverlay.addTo(M.map);// Ajout à la carte Leaflet
+// });
