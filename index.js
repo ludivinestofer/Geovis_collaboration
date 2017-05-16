f1da4927c096f1724e3f602412ba2faedf6c433a
Christian Kaiser <ck@naxio.ch>
2017-05-16 19:54:09 +0200

    Carte des communes dans Leaflet sans couleurs

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 var M = {
   bbox: [485000, 75000, 834000, 296000],
   data: {}, dataSeries: [],
+  proj: proj4("+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=600000 +y_0=200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs"),
 };
 
 // Définition de la carte en SVG avec le cadre notamment
 function main(){
-  M.svg = d3.select("svg.choropleth");
-  M.width = M.svg.attr('width');
-  M.height = M.svg.attr('height');
-
-// Définition d'une géométrie géoJSON en SVG
-  M.path = d3.geoPath();
+  M.map = L.map('mapdiv').setView([46.9, 7.9], 8);
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(M.map);
 
   // Il faut créer une sorte de boucle (queue) qui permet de charger les données ainsi que les shapefiles
   d3.queue()
@@ -33,67 +32,39 @@ function main(){
 function drawMap(error, data){
   if (error) throw error;
 
-  // The TopoJSON contains raw coordinates in CRS CH1903/LV03.
-  // As this is already a projected CRS, we can use an SVG transform
-  // to fit the map into the SVG view frame.
-  // In a first step, we compute the transform parameters.
+  M.communesOverlay = L.d3SvgOverlay(function(sel, proj){
 
-  // Compute the scale of the transform
-  var scaleX = M.width / (M.bbox[2] - M.bbox[0]),
-      scaleY = M.height / (M.bbox[3] - M.bbox[1]);
-  var scale = Math.min(scaleX, scaleY); // Pour garder la même échelle en x et en y afin d'éviter les déformations
+    // Ci-dessous le code qui permet de faire une projection
+    // de CH1903 à WGS84 directement dans le navigateur.
+    // Ce n'est pas optimal, car un peu lent, mais ça fonctionne.
+    var projectPoint = function(x,y){
+      var pt_wgs84 = M.proj.inverse([x,y]);
+      var pt = M.map.latLngToLayerPoint(new L.LatLng(pt_wgs84[1], pt_wgs84[0]));
+      this.stream.point(pt.x, pt.y);
+    };
+    var geopath = proj.pathFromGeojson
+      .projection(d3.geoTransform({point: projectPoint}));
+    // Fin du code de projection.
 
-  var dx = -1 * scale * M.bbox[0];
-  var dy = scale * M.bbox[1] + parseFloat(M.height);
+    var features = sel.selectAll('path')
+      .data(topojson.feature(data, data.objects.communes).features);
 
-  M.map = M.svg.append('g')
-    .attr('class', 'map')
-    .attr(
-      'transform',
-      'matrix('+scale+' 0 0 -'+scale+' '+dx+' '+dy+')'
-    );
+    features
+      .enter()
+      .append('path')
+      .attr('stroke','white')
+      .attr('fill', 'red')
+      .attr('fill-opacity', 0.5)
+      .attr('d', geopath);
+      // Si on ne doit pas projeter les coordonnées dans le
+      // navigateur, il faut simplement remplacer la dernière
+      // ligne ci-dessus par celle ci-dessous:
+      //.attr('d', proj.pathFromGeojson);
 
-  // Mise en classe de la carte avec Jenks.
-  M.brew = new classyBrew();
-  M.brew.setSeries(M.dataSeries);
-  M.brew.setNumClasses(6);
-  M.brew.setColorCode('PuBu');
-  M.breaks = M.brew.classify('jenks');
+    features
+      .attr('stroke-width', 0.3 / proj.scale);
 
-// Sélection des classes et des couleurs
-  M.color = d3.scaleThreshold()
-    .domain(M.breaks.slice(1,6))
-    .range(M.brew.getColors());
+  });
 
-  // Transformation des features en geoJSON et en SVG
-  M.map
-    .append('g').attr('class', 'communes')
-    .selectAll('path')
-    .data(topojson.feature(data, data.objects.communes).features) // Utilisation et conversion du TopoJSON
-    .enter()
-    .append('path')
-    .attr('fill', function(d){
-      return M.data[d.properties.id] ?
-        M.color(M.data[d.properties.id].p_fem_singl_2034) :
-        '#fff'; // Code couleur pour les données manquantes. 
-    })
-    .attr('d', M.path);
-
-  // Limites des cantons tracées en blanc. Attention le stroke-width est en mètres !!
-  M.map
-    .append('g').attr('class', 'cantons')
-    .selectAll('path')
-    .data(topojson.feature(data, data.objects.cantons).features)
-    .enter()
-    .append('path')
-    .attr('stroke', '#fff').attr('stroke-width', '200')
-    .attr('fill', 'none').attr('d', M.path);
-
-  // La couche des lacs est en dernier afin qu'ils apparaissent dessus.
-  M.map
-    .append('g').attr('class', 'lacs')
-    .selectAll('path')
-    .data(topojson.feature(data, data.objects.lacs).features)
-    .enter().append('path')
-    .attr('fill', '#777').attr('d', M.path);
+  M.communesOverlay.addTo(M.map);
 }
